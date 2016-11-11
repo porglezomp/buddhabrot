dab111655f1cb89bdc7e63c66a6e7f6d99515f57
Caleb Jones <code@calebjones.net>
2016-11-10 20:37:55 -0500

    Implement multithreaded rendering

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ use std::path::Path;
 use std::ops::{Add, Sub, Mul};
 use rand::{Rand, Rng};
 use rand::distributions::{IndependentSample, Range};
-use std::io::Write;
+use std::thread;
+use std::sync::{Arc, Mutex};
 
 
 #[derive(PartialEq, Clone, Copy)]
@@ -128,58 +129,83 @@ fn main() {
     let (r, g, b) = (20, 200, 2000);
     let width = 2048;
     let height = 2048;
-    let n_iters = 1000000;
-    let mut red = Buffer::new(width, height);
-    let mut green = Buffer::new(width, height);
-    let mut blue = Buffer::new(width, height);
+    let n_iters = 10000000;
+    let n_threads = 4;
+    struct Res {
+        red: Buffer,
+        green: Buffer,
+        blue: Buffer,
+    }
+    let results = Arc::new(Mutex::new(Vec::new()));
+    let mut threads = Vec::new();
+    for _ in 0..n_threads {
+        let res = results.clone();
+        threads.push(thread::spawn(move || {
+            let mut red = Buffer::new(width, height);
+            let mut green = Buffer::new(width, height);
+            let mut blue = Buffer::new(width, height);
 
-    for i in 0..n_iters {
-        for _ in 0..(1000/r)+1 {
-            buddhabrot(&mut red, r);
-        }
-        for _ in 0..(1000/g)+1 {
-            buddhabrot(&mut green, g);
-        }
-        for _ in 0..(1000/b)+1 {
-            buddhabrot(&mut blue, b);
-        }
-        if i % 1000 == 0 {
-            print!("\r{:3}%", i * 100 / n_iters);
-            std::io::stdout().flush().unwrap();
+            for _ in 0..(n_iters/n_threads) {
+                for _ in 0..(1000/r)+1 {
+                    buddhabrot(&mut red, r);
+                }
+                for _ in 0..(1000/g)+1 {
+                    buddhabrot(&mut green, g);
+                }
+                for _ in 0..(1000/b)+1 {
+                    buddhabrot(&mut blue, b);
+                }
+            }
+
+            let mut res = res.lock().unwrap();
+            res.push(Res {
+                red: red,
+                green: green,
+                blue: blue,
+            });
+        }));
+    }
+
+    for thread in threads { thread.join().unwrap(); }
+
+    let size = width * height;
+    let mut reds = vec![0; size];
+    let mut greens = vec![0; size];
+    let mut blues = vec![0; size];
+
+    let results = results.lock().unwrap();
+    for result in results.iter() {
+        for i in 0..size {
+            reds[i] += result.red.buffer[i];
+            greens[i] += result.green.buffer[i];
+            blues[i] += result.blue.buffer[i];
         }
     }
 
     let zero = 0;
-    let r_min = red.buffer.iter().min().unwrap_or(&zero);
-    let r_max = red.buffer.iter().max().unwrap_or(&zero);
+    let r_min = reds.iter().min().unwrap_or(&zero);
+    let r_max = reds.iter().max().unwrap_or(&zero);
     let r_range = if r_min == r_max { 1 } else { r_max - r_min };
-    let g_min = green.buffer.iter().min().unwrap_or(&zero);
-    let g_max = green.buffer.iter().max().unwrap_or(&zero);
+    let g_min = greens.iter().min().unwrap_or(&zero);
+    let g_max = greens.iter().max().unwrap_or(&zero);
     let g_range = if g_min == g_max { 1 } else { g_max - g_min };
-    let b_min = blue.buffer.iter().min().unwrap_or(&zero);
-    let b_max = blue.buffer.iter().max().unwrap_or(&zero);
+    let b_min = blues.iter().min().unwrap_or(&zero);
+    let b_max = blues.iter().max().unwrap_or(&zero);
     let b_range = if b_min == b_max { 1 } else { b_max - b_min };
 
-    let size = red.buffer.len();
     let mut buffer = Vec::with_capacity(3 * size);
     for i in 0..size {
-        buffer.push(((red.buffer[i] - r_min) * 255 / r_range) as u8);
-        buffer.push(((green.buffer[i] - g_min) * 255 / g_range) as u8);
-        buffer.push(((blue.buffer[i] - b_min) * 255 / b_range) as u8);
+        buffer.push(((reds[i] - r_min) * 255 / r_range) as u8);
+        buffer.push(((greens[i] - g_min) * 255 / g_range) as u8);
+        buffer.push(((blues[i] - b_min) * 255 / b_range) as u8);
     }
 
     image::save_buffer(Path::new("image.png"),
                        &buffer,
-                       red.width as u32,
-                       red.height as u32,
+                       width as u32,
+                       height as u32,
                        image::RGB(8))
         .unwrap();
     println!("\r100%");
-    println!("R: {} {}\nG: {} {}\nB: {} {}",
-             red.buffer.iter().min().unwrap_or(&0),
-             red.buffer.iter().max().unwrap_or(&0),
-             green.buffer.iter().min().unwrap_or(&0),
-             green.buffer.iter().max().unwrap_or(&0),
-             blue.buffer.iter().min().unwrap_or(&0),
-             blue.buffer.iter().max().unwrap_or(&0));
+    println!("R: {} {}\nG: {} {}\nB: {} {}", r_min, r_max, g_min, g_max, b_min, b_max);
 }
